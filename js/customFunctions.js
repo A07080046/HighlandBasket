ca8e0e7db36dc7edd16e7bd08a9ade21b6093286
honggaoGong <ghg@iMac.local>
2015-11-10 14:55:35 +0800

    完善框架

---
+++ js/customFunctions.js
@@ -0,0 +1,83 @@
+/**
+ * Created by ghg on 15/11/10.
+ */
+function clickHomePage()
+{
+    var x = document.getElementById('comeon');
+    for (var i=0;i<15;i++) {
+        var html = "<div class='span2' id='goods1'><div class='thumbnail'><a href='http://codeguide.bootcss.com' title='Bootstrap 编码规范' target='_blank' onclick='_hmt.push(['_trackEvent', 'tile', 'click', 'codeguide'])'><img class='lazy' src='img/4.jpg' width='300' height='150'' data-src='http://static.bootcss.com/www/assets/img/codeguide.png' alt='Headroom.js'></a><div class='caption'><h3><a href='http://codeguide.bootcss.com' title='Bootstrap 编码规范：编写灵活、稳定、高质量的 HTML 和 CSS 代码的规范。' target='_blank' onclick='_hmt.push(['_trackEvent', 'tile', 'click', 'codeguide'])'>大豆芽<br><small>贵州大山生长</small></a></h3><p>大山里种的，没用农业，常年非大棚种植，欢迎选购。</p></div></div></div>";
+        $(html).appendTo(x);
+    }
+}
+
+function clickVegetable()
+{
+
+
+}
+
+function clickFreshFruit()
+{
+
+
+}
+
+function clickFreshMeat()
+{
+
+
+}
+
+function clickNut()
+{
+
+
+}
+
+function clickWildHerbs()
+{
+
+
+}
+
+function clickWildFruit()
+{
+
+
+}
+
+function clickGame()
+{
+
+
+}
+
+function clickSearch()
+{
+
+}
+
+function clickBasket()
+{
+
+}
+
+function clickFavorite()
+{
+
+}
+
+function clickOrder()
+{
+
+}
+
+function clickAccount()
+{
+
+}
+
+function clickLogin()
+{
+
+}
